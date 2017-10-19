4c74abca21a09f82acebbe7e796b7a16d6287e5d
Anubiella <rathvelcar@libero.it>
2017-10-19 11:51:21 +0200

    Create Ajax request to last.fm

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+    $('.search').click(function(){
+    	Trackster.searchTracksByTitle($('#inputext').val());
+    });
+
+
+const API_KEY = '7ade5558956123a107411dfabf1e2772';
 var Trackster = {};
 
 /*
@@ -13,5 +20,12 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+	$.ajax({
+		url:'http://ws.audioscrobbler.com/2.0/?method=track.search&track='+title+'&api_key='+API_KEY+'&format=json',
+		success: function(result){
+			console.log(result);
+		}
+	});
 };
+
+});
